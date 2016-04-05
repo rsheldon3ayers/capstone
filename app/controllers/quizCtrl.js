642b5c30f8e20797365aaa73afcc3a98f32f1b27
Robert Ayers <rsheldon3ayers@gmail.com>
2016-04-05 16:59:51 -0500

    added package.json

--- app/controllers/quizCtrl.js
+++ app/controllers/quizCtrl.js
@@ -1,105 +1,107 @@
-app.controller("quizCtrl", ["$scope", "$firebaseAuth","$firebaseArray", "$location", "getBookObj", 
-	function ($scope, $firebaseAuth, $firebaseArray, $location, getBookObj) {
-		var randomWord;
-		var bookID = getBookObj.getBookID();
-		console.log("bookID in quizCtrl", bookID);
+app.controller("quizCtrl", ["$scope", "$firebaseAuth", "$firebaseArray", "$location", "getBookObj",
+    function($scope, $firebaseAuth, $firebaseArray, $location, getBookObj) {
+        var randomWord;
+        var bookID = getBookObj.getBookID();
+        console.log("bookID in quizCtrl", bookID);
 
-		var ref = new Firebase('https://co-read-quiz.firebaseio.com/' + bookID + "/noteDetail");
-		
-		var notes = $firebaseArray(ref);
-		var keepWords = ["am", "A", "are", "is", "was", "were", "be", "being", "been", 
-							"have", "has", "had", "shall", "will", "do", "does", "did", 
-							"may", "must", "might", "can", "can't", "could", "would", "should",
-							 "a", "an", "the", "and", "or", "some", "here", "who", "what", 
-							 "when", "where", "how", "why", "in", "There", "When", "This"];
-		notes.$loaded(
+        var ref = new Firebase('https://co-read-quiz.firebaseio.com/' + bookID + "/noteDetail");
 
-			function findWord (notes){
-				
-				var newArray = [];
-				
-				$scope.newNotes = {};
-				
-				console.log($scope.newNotes);
-				console.log(notes[0].$value);
-				
+        var notes = $firebaseArray(ref);
+        var keepWords = ["am", "A", "are", "is", "was", "were", "be", "being", "been",
+            "have", "has", "had", "shall", "will", "do", "does", "did",
+            "may", "must", "might", "can", "can't", "could", "would", "should",
+            "a", "an", "the", "and", "or", "some", "here", "who", "what",
+            "when", "where", "how", "why", "in", "There", "When", "This"
+        ];
+        notes.$loaded(
 
-			for (var i = 0; i < notes.length; i++){
+            function findWord(notes) {
 
-				var filterNotes = notes[i].$value;
+                var newArray = [];
 
-				var stringArray = filterNotes.split(" ");
+                $scope.newNotes = {};
 
-				// var randomWord = stringArray[Math.floor(Math.random() * stringArray.length)];
-				
-				
+                console.log($scope.newNotes);
+                console.log(notes[0].$value);
 
-				stringArray.forEach(function(stringArrayWord, index, stringArray) {
-					
-					if (keepWords.includes(stringArrayWord) || stringArray.includes('____')) {
-						
 
-						//do nothing at all
+                for (var i = 0; i < notes.length; i++) {
+
+                    var filterNotes = notes[i].$value;
+
+                    var stringArray = filterNotes.split(" ");
+
+                    // var randomWord = stringArray[Math.floor(Math.random() * stringArray.length)];
 
 
 
-					} else {
-						randomWord = stringArray[index];
-						console.log("use this to check answer", randomWord)
-						stringArray[index] = '____';
-						var newString = stringArray.join();
-					
-						var noComma = newString.replace(/,/g, " ");
-						console.log(noComma);
-						newArray.push({noComma, randomWord});
-						console.log(newArray);
+                    stringArray.forEach(function(stringArrayWord, index, stringArray) {
+
+                        if (keepWords.includes(stringArrayWord) || stringArray.includes('____')) {
 
 
-						return $scope.newNotes = newArray;
-						console.log("next up!")
-
-					}
-				})
-
-		
-		};	
-		$scope.wrongAnswer = false;
-		$scope.correctAnswer = false;
+                            //do nothing at all
 
 
 
+                        } else {
+                            randomWord = stringArray[index];
+                            console.log("use this to check answer", randomWord)
+                            stringArray[index] = '____';
+                            var newString = stringArray.join();
 
-			
-	})
-	$scope.noteIndex;
-	$scope.checkAnswer = function (userAnswer, randomWord){
-			
-			console.log("check works");
-			
-		
-			if(userAnswer === randomWord){
-			
-				$scope.correctAnswer = true;
-				$scope.wrongAnswer = false;
-				 // $location.path('/QuizApp/notes');
+                            var noComma = newString.replace(/,/g, " ");
+                            console.log(noComma);
+                            newArray.push({ noComma, randomWord });
+                            console.log(newArray);
 
-			 
-		} else {
-			
-			$scope.wrongAnswer = true;
-			$scope.correctAnswer = false;
 
-			console.log("if/else works, right answer");
-		}
-	}
-	
-	$scope.onSubmit = function () {
+                            return $scope.newNotes = newArray;
+                            console.log("next up!")
 
-		 $location.path('/QuizApp/notes');
-	};
-	$scope.onView = function () {
+                        }
+                    })
 
-		 $location.path('/QuizApp/noteView');
-	};
-	
-}]); //end of contoller		
+
+                };
+                $scope.wrongAnswer = false;
+                $scope.correctAnswer = false;
+
+
+
+
+
+            })
+        $scope.noteIndex;
+        $scope.checkAnswer = function(userAnswer, randomWord) {
+
+            console.log("check works");
+
+
+            if (userAnswer === randomWord) {
+
+                $scope.correctAnswer = true;
+                $scope.wrongAnswer = false;
+                // $location.path('/QuizApp/notes');
+
+
+            } else {
+
+                $scope.wrongAnswer = true;
+                $scope.correctAnswer = false;
+
+                console.log("if/else works, right answer");
+            }
+        }
+
+        $scope.onSubmit = function() {
+
+            $location.path('/QuizApp/notes');
+        };
+        $scope.onView = function() {
+
+            $location.path('/QuizApp/noteView');
+        };
+
+    }
+]); //end of contoller
--- app/factories/getBookObj.js
+++ app/factories/getBookObj.js
@@ -1,33 +1,32 @@
-
 app.factory("getBookObj", [function() {
 
-  var bookID = "";
-  return {
-    getBookID: function() {
+    var bookID = "";
+    return {
+        getBookID: function() {
 
-      return bookID;
-    },
-    setBookID: function(value) {
-      bookID = value;
-      return bookID;
+            return bookID;
+        },
+        setBookID: function(value) {
+            bookID = value;
+            return bookID;
 
-    },
-    setDetailID: function(value) {
-      detailID = value;
-      return detailID;
-    },
-    getDetailID: function() {
-      return detailID;
-    },
-    setNoteID: function(value) {
-      noteID = value;
-      return noteID;
-    },
-    getNoteID: function() {
-      return noteID;
+        },
+        setDetailID: function(value) {
+            detailID = value;
+            return detailID;
+        },
+        getDetailID: function() {
+            return detailID;
+        },
+        setNoteID: function(value) {
+            noteID = value;
+            return noteID;
+        },
+        getNoteID: function() {
+            return noteID;
+        }
+
+
+
     }
-
-
-
-  }
 }]); //end of factory
